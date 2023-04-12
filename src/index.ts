b6b32e7aaf88dac8b7fbfe9d9d16bc9d503c54c4
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-04-11 22:15:07 -0400

    Typing Command

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,10 @@
 import 'colors';
 import type { Noop, IObject } from '@arcaelas/utils'
 
-type CommandOptionsArguments<T extends IObject = IObject> = {
-    [K in keyof T]: string | number | boolean | Noop | {
-        /**
-         * @description
-         * Short description about this option to show when help command is run on this command.
-         */
-        description?: string
-        /** 
-         * @description
-         * Function to parse value from argument list.
-         */
-        type?: Noop
-        /**
-         * @description
-         * Statics props can't be change
-         */
-        static?: string | number | boolean | null
-        /**
-         * @description
-         * Defult value to use when argument list dont have any value for this argument.
-         */
-        value?: string | number | boolean | null
-    }
-}
+type Inmutables = string | number | boolean
 
-type CommandOptionsActionOptions<T extends CommandOptionsArguments = CommandOptionsArguments> = {
-    [K in keyof T]: T[K] extends Noop ? ReturnType<T[K]> : (
-        T[K] extends IObject ? (
-            T[K]["type"] extends Noop ? ReturnType<T[K]["type"]> : (
-                T[K]["static"] extends never ? T[K]["value"] : T[K]["static"]
-            )
-        ) : T[K]
-    )
-}
 
-interface CommandOptions<T extends CommandOptionsArguments = CommandOptionsArguments> {
-    /**
-     * @deprecated
-     * @description
-     * Other names you want to use to access this command.
-     * @example
-     * command({
-     *  alias: ["dev", "run", "serve"]
-     * })
-     * command.exec("dev", ...args)
-     */
-    alias?: string[]
+interface CommandOptions {
     /**
      * @description
      * Short description about this command to show on help command run.
@@ -58,44 +15,57 @@ interface CommandOptions<T extends CommandOptionsArguments = CommandOptionsArgum
      * List of arguments that will be passed to the command from the execution line.
      * NOTE: Each argument will be processed before the command is executed. 
      */
-    arguments?: T
-    action(options: CommandOptionsActionOptions<T>, argv: string[]): void
+    arguments?: {
+        [K: string | number]: Inmutables | Noop<string, Inmutables> | {
+            /**
+             * @description
+             * Short description about this option to show when help command is run on this command.
+             */
+            description?: string
+            /** 
+             * @description
+             * Function to parse value from argument list.
+             */
+            type?: Noop<string, Inmutables>
+            /**
+             * @description
+             * Statics props can't be change
+             */
+            static?: Inmutables
+            /**
+             * @description
+             * Defult value to use when argument list dont have any value for this argument.
+             */
+            value?: Inmutables
+        }
+    }
+    action(options: ParseArguments<this>, argv: string[]): void
 }
 
-export default interface Command<T extends CommandOptionsArguments = CommandOptionsArguments> {
-    new(options: CommandOptions<T>): any
 
-    /**
-     * @description
-     * Show command usage and arguments descriptions in console.
-     */
-    help(): void
-
-    /**
-     * @description
-     * Run the command with a given list of arguments that will be formatted according to the argument configuration.
-     * @example
-     * const start = new Command("serve", {...})
-     * 
-     * start.exec("--port 8080")
-     * start.exec("--port", "8080")
-     * start.exec(["--port 8080"])
-     * start.exec(["--port", "8080"])
-     */
-    exec(...argv: Array<string | string[]>): any
+type ParseArguments<O extends CommandOptions = CommandOptions, T extends CommandOptions["arguments"] = O["arguments"]> = {
+    [K in keyof T]: T[K] extends Noop ? ReturnType<T[K]> : (
+        T[K] extends IObject<Noop> ? (
+            T[K]["type"] extends Noop ? ReturnType<T[K]["type"]> : (
+                T[K]["static"] extends never ? T[K]["value"] : T[K]["static"]
+            )
+        ) : T[K]
+    )
 }
 
-export default class Command<T extends CommandOptionsArguments = CommandOptionsArguments> {
-    private params: IObject = {}
-    constructor(private options: CommandOptions<T>) {
-        options.arguments ??= {} as any
-        for (const key in options.arguments) {
-            const value = typeof options.arguments[key] === "function" ? { type: options.arguments } : (
-                typeof (options.arguments[key] ?? false) !== "object" || Array.isArray(options.arguments[key])
-                    ? { value: options.arguments[key] } : options.arguments[key]
-            ) as IObject
-            this.params[key] = {
-                value: value?.static ?? value?.value ?? null,
+
+export default class Command {
+    private params:IObject<Noop | Function> = {}
+
+    constructor(private options: CommandOptions) {
+        options.arguments ??= {}
+        for(const key in options.arguments){
+            const value: IObject<Noop> = typeof options.arguments[key] === "function" ? { type: options.arguments[key] } : (
+                typeof(options.arguments[key]??false)!=="object" || Array.isArray(options.arguments[key])
+                    ? { value: options.arguments[key] } : options.arguments[key] as IObject
+            )
+            this.params[ key ] = {
+                value: value?.static ?? value.value ?? false,
                 description: value?.description ?? 'N/A',
                 type: value?.type ?? value.value?.constructor ?? (v => v),
                 set(v) {
@@ -111,6 +81,17 @@ export default class Command<T extends CommandOptionsArguments = CommandOptionsA
         }
     }
 
+    /**
+     * @description
+     * Run the command with a given list of arguments that will be formatted according to the argument configuration.
+     * @example
+     * const start = new Command("serve", {...})
+     * 
+     * start.exec("--port 8080")
+     * start.exec("--port", "8080")
+     * start.exec(["--port 8080"])
+     * start.exec(["--port", "8080"])
+     */
     async exec(...argv: Array<string | string[]>) {
         argv = argv.flat(Infinity).map(e => String(e).trim()).filter(Boolean)
         let last
@@ -118,28 +99,30 @@ export default class Command<T extends CommandOptionsArguments = CommandOptionsA
             let [, arg] = k.match(/^--([a-z][\w-_]{2,})/i) || []
             if (arg) last = arg
             else if (last in this.params) {
-                this.params[last].set(k)
-                if (this.params[last].type !== Array) last = undefined
+                (this.params[last] as any).set(k)
+                if ((this.params[last] as any).type !== Array) last = undefined
             }
         }
         const proxy = new Proxy({}, {
-            get: (_, k) => this.params[k]?.value,
+            get: (_, k) => (this.params[k] as any)?.value,
         })
         return this.options.action(proxy as any, argv as string[])
     }
 
-    help() {
+    /**
+     * @description
+     * Show command usage and arguments descriptions in console.
+     */
+    help(): void {
         console.log(`Arcaelas Insiders CLI`.green.bold);
         console.log("%s", this.options.usage || 'N/A')
         console.log("arguments:".yellow.bold)
         for (let k in this.params) {
-            let option = this.params[k];
+            let option: any = this.params[k];
             console.log(`   --${k}`);
             console.log(`       %s`, option.description);
         }
     }
 }
 
-export function command<T extends CommandOptionsArguments = CommandOptionsArguments>(options: CommandOptions<T>): Command<T> {
-    return new Command(options)
-}
+
