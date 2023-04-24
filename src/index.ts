39bd30363d7a5f4e530809fae2f7a211d3a93833
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-04-24 02:20:51 -0400

    ---

--- src/index.ts
+++ src/index.ts
@@ -143,6 +143,8 @@ export default class Command<R = any, T extends CommandArguments = CommandArgume
             }
         }
         for (const key in this.types) {
+            console.log('Key: %s, Type: %s', key, this.types[key].type)
+
             props[key] = await (key in props ? (
                 this.types[key].type === Array
                     ? [].concat(props[key])
