8fd15e4c19a0c7eb2fdd6390d74e17943757891d
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-05-08 14:23:39 -0400

    EsBuild as CommonJS

--- esbuild.js
+++ esbuild.js
@@ -1,20 +1,40 @@
 #! ts-node
 const { build } = require("esbuild")
+const { promises } = require("fs")
+
+function naming(dirname) {
+    return {
+        name: "resolution_name",
+        setup(build) {
+            build.onLoad({ filter: /\.test\.ts$/ }, async args => {
+                const content = await promises.readFile(args.path, 'utf-8')
+                return {
+                    contents: content.replace("../src/", dirname),
+                    loader: "ts",
+                }
+            })
+        }
+    }
+}
 
 build({
-    entryPoints: ["test/index.test.ts"],
-    outdir: "test",
-    bundle: false,
+    entryPoints: ["./src/index.ts"],
+    outfile: "./lib/index.js",
+    bundle: true,
     minify: true,
-    platform: "node",
+    platform: "neutral",
     format: "cjs",
 })
 
+// Test Files
 build({
-    entryPoints: ["src/index.ts"],
-    outdir: "lib",
+    entryPoints: ["./test/index.test.ts"],
+    outfile: "./test/index.test.js",
     bundle: false,
-    minify: true,
-    platform: "node",
+    minify: false,
+    platform: "neutral",
     format: "cjs",
+    plugins: [
+        naming('../lib'),
+    ]
 })
